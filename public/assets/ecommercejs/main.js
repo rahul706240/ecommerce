a420bc7c227fc2a6d7343ae7533e1c352ea6587c
unknown <rajt02489@gmail.com>
2023-01-23 23:34:34 +0530

    monday night 11:34pm

---
+++ public/assets/ecommercejs/main.js
@@ -0,0 +1,33 @@
+
+        $(document).ready(function () {
+            $('#autoWidth').lightSlider({
+                autoWidth: true,
+                loop: true,
+                onSliderLoad: function () {
+                    $('#autoWidth').removeClass('cS-hidden');
+                }
+            });
+        });
+    
+    
+        $(document).ready(function () {
+            $('#autoWidthtwo').lightSlider({
+                autoWidth: true,
+                loop: true,
+                onSliderLoad: function () {
+                    $('#autoWidthtwo').removeClass('cS-hiddentwo');
+                }
+            });
+        });
+    
+    
+        $(document).ready(function () {
+            $('#autoWidththree').lightSlider({
+                autoWidth: true,
+                loop: true,
+                onSliderLoad: function () {
+                    $('#autoWidththree').removeClass('cS-hiddenthree');
+                }
+            });
+        });
+    
